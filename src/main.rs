bb082fd0f64b32e5e56ffb9911857f558cdb1fef
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2022-08-05 22:40:38 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+// Copy files from one folder to another
+// Folders can be on the same or different network
+// Folders can be on the same or different computer
+// Folders can be nested
+// Source and Destination should be configurable and read at runtime
+extern crate chrono;
+
+use std::fs;
+use std::path::Path;
+use walkdir::WalkDir;
+
+struct Config {
+    source: String,
+    destination: String,
+}
+
+fn parse_config() -> Config {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 4 {
+        panic!("Usage: cp <source> <destination>\n");
+    }
+    Config {
+        source: args[2].clone(),
+        destination: args[3].clone(),
+    }
+}
+
+fn copy_file(source: &Path, destination: &Path) {
+    fs::copy(source, destination).unwrap();
+}
+
+fn copy_folder(source: &Path, destination: &Path) {
+    fs::create_dir_all(destination).unwrap();
+    for entry in WalkDir::new(source) {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_file() {
+            copy_file(&path, &destination.join(path.file_name().unwrap()));
+        } else if path.is_dir() {
+            fs::create_dir_all(&destination.join(path.file_name().unwrap())).unwrap();
+        }
+    }
+}
+
+fn copy_files(config: &Config) {
+    let source = Path::new(&config.source);
+    let destination = Path::new(&config.destination);
+    if source.is_file() {
+        copy_file(&source, &destination);
+    } else if source.is_dir() {
+        copy_folder(&source, &destination);
+    } else {
+        panic!("Source is not a file or directory\n");
+    }
+}
+
+use std::thread::sleep;
+use std::time::Duration;
+use chrono::offset::Utc;
+use chrono::DateTime;
+
+fn main() {
+    let config = parse_config();
+
+    // run the copy_files function every 60 seconds
+    loop {
+        // print start time hh:mm:ss
+        let start = std::time::SystemTime::now();
+        let start_utc: DateTime<Utc> = start.into();
+        print!("Start: {}\n", start_utc.format("%H:%M:%S"));
+
+        // copy files from source to destination
+        copy_files(&config);
+
+        // print elapsed time
+        let end = std::time::SystemTime::now();
+        let duration = end.duration_since(start).unwrap();
+        print!("Elapsed: {} seconds\n", duration.as_secs());
+
+        // print a message to the console
+        println!("Copied files\n");
+
+        // wait 15minutes
+        sleep(Duration::new(900, 0));
+    }    
+}
